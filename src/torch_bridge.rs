4afb7cffe1ecbb93b77d1c501c1c072485700e79
Jia Liu <proljc@gmail.com>
2026-07-09 18:59:20 +0800

    Add license header comments to all source files

--- src/torch_bridge.rs
+++ src/torch_bridge.rs
@@ -1,10 +1,25 @@
+// InferTrain - A Unified Inference and Training Engine
+//
+// Copyright (c) 2026 Jia Liu & InferTrain Contributors
+// SPDX-License-Identifier: Apache-2.0
+//
+// This file is part of InferTrain.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use pyo3::prelude::*;
-// use pyo3::types::PyTuple;
 use pyo3::types::{PyAny, PyList};
 
 use crate::frontend::hook::HookTracer;
-// use crate::ir::dag::DagGraph;
-// use crate::ir::serialize::PyDagGraph;
 use crate::executor::PyExecutor;
 
 /// Trace from PyTorch model and return DAG
